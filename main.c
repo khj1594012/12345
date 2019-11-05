3657c3ce1707ade800c8be8c5ef9b6bef588cdf0
khj <khj1594012@gmail.com>
2019-11-05 14:17:25 +0900

    initial commit

---
+++ main.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<windows.h>
+
+int main(){
+
+	//1.새로운 학생 목록 생성 2.이미있는파일열기
+	int select;
+
+	printf("1.새로운 파일 생성  2. 파일 열기 3. 종료\n");
+
+	while(1) {
+		scanf("%d", &select);
+
+		if(select == 1){
+
+
+
+		}
+
+		else if(select == 2){
+
+
+
+		}
+
+		else if(select == 3){
+
+			printf("종료\n");
+			break;
+		}
+
+		else
+			printf("잘못입력\n");
+
+	}
+
+
+	getch();
+
+	return 0;
+}
+
+
